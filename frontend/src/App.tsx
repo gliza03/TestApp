171253b6a98333b2291e15a407a50cf4679ee9a2
gabe <gabrielliza012@gmail.com>
2025-03-08 17:24:34 -0700

    Finished assignment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,49 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import teams from './CollegeBasketballTeams.json';
 // This is typescript!
 function Welcome() {
-  return <h1>Criminally underrated pop girls</h1>;
-}
-
-const artists = [
-  { name: 'Addison Rae', bestSong: 'Diet Pepsi' },
-  { name: 'Tinashe', bestSong: 'Nasty' },
-  { name: 'LISA', bestSong: 'Rockstar' },
-];
-
-function Band({ name, bestSong }: { name: string; bestSong: string }) {
   return (
-    <>
-      <h2>Name: {name}</h2>
-      <h2>Song: {bestSong}</h2>
-    </>
+    <div>
+      <h1>March Madness!</h1>
+      <h3>
+        With March Madness coming up, here's a website that lists out info about
+        all colleges in NCAA Basketball!
+      </h3>
+    </div>
   );
 }
 
-function BandList() {
+function Team(props: {
+  tid: number;
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}) {
   return (
-    <>
-      {artists.map((oneArtist) => (
-        <Band {...oneArtist} />
-      ))}
-    </>
+    <div className="card m-3 shadow" style={{ width: '18rem' }}>
+      <div className="card-body">
+        <h5 className="card-title">{props.school}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{props.name}</h6>
+        <p className="card-text">
+          <strong>Location:</strong> {props.city}, {props.state}
+        </p>
+      </div>
+    </div>
+  );
+}
+function TeamList() {
+  return (
+    <div className="container">
+      <div className="row">
+        {teams.teams.map((oneTeam) => (
+          <div className="col-md-4">
+            <Team {...oneTeam} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
@@ -33,7 +51,7 @@ function App() {
   return (
     <>
       <Welcome />
-      <BandList />
+      <TeamList />
     </>
   );
 }
